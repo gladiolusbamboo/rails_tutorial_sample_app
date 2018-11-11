9c9797bdafbfd42944a3fe6cfd0c06852787a33a
Yuji Koyama <dummy@gladiolus.egoism.jp>
2018-11-11 05:52:07 +0000

    181111 Rails チュートリアル

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  # /にGETでアクセスするとstatic_pagesコントローラーのhomeメソッドを実行し、
+  # 対応するViewで描画する
+  root 'static_pages#home'
+
+  # それぞれのURLにGETでアクセスすると
+  # 対応するコントローラーのメソッドを実行し、
+  # 対応するViewで描画する
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
-  root 'static_pages#home'
 end
