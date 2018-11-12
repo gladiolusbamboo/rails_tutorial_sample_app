d5ffc4f49018d63dc7171b24cf9c7378984bf15d
Yuji Koyama <dummy@gladiolus.egoism.jp>
2018-11-12 06:02:41 +0000

    181112 Rails Tutorial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
+# ここに汎用的なヘルパーメソッドを書いていく
 module ApplicationHelper
+  # ページごとの完全なタイトルを返します。
+  # デフォルト引数を使用する
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,27 +1,38 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  # 各テストの前に実行されるメソッド
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
   
+  # ルートURLにアクセスしたときにレスポンスが返ってくるか
   test "should get root" do
     get root_url
     assert_response :success
   end
   
+  # static_pagesコントローラーのhomeアクションに対応するURLにアクセスしたときに
+  # レスポンスが返ってくるか
+  # <title>タグの内容は"Home | Ruby on Rails Tutorial Sample App"であるか
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
+  # static_pagesコントローラーのhelpアクションに対応するURLにアクセスしたときに
+  # レスポンスが返ってくるか
+  # <title>タグの内容は"Help | Ruby on Rails Tutorial Sample App"であるか
   test "should get help" do
     get static_pages_help_url
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
   
+  # static_pagesコントローラーのaboutアクションに対応するURLにアクセスしたときに
+  # レスポンスが返ってくるか
+  # <title>タグの内容は"About | Ruby on Rails Tutorial Sample App"であるか
   test "should get about" do
     get static_pages_about_url
     assert_response :success
