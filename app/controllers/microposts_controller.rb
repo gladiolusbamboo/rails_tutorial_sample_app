0445f972c63cb68eafca2a71f0154381cc04b85f
Yuji Koyama <dummy@gladiolus.egoism.jp>
2018-12-18 06:38:02 +0000

    181218 Rails Tutorial

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -3,9 +3,27 @@ class MicropostsController < ApplicationController
   # logged_in_userメソッドを実行し、ログイン済みかを確認する
   before_action :logged_in_user, only: [:create, :destroy]
 
+  # microposts POST   /microposts(.:format)     microposts#create
   def create
+    # ポストを作成する
+    # XXX_paramsはStrongParametersを使用するときの常套句
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      # 再描画？
+      render 'static_pages/home'
+    end
   end
 
   def destroy
   end
+  
+  private
+    # Strong Parameters用
+    def micropost_params
+      # Micropostはcontentのみ変更を許可する
+      params.require(:micropost).permit(:content)
+    end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
+    # ログイン中ならユーザーと紐付いたポストを新規作成する
+    # (フォームの作成に必要)
+    # /sample_app/app/views/shared/_micropost_form.html.erb
+    # で参照している
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def help
